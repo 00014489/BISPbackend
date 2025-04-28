b181bac98174bb258db7d18f04a86a9014e7a98f
Usmonov Elbek <usmonovelbek7@gmail.com>
2025-04-28 08:08:53 +0500

    I think final changes

--- testing2timesTelegram/Program.cs
+++ testing2timesTelegram/Program.cs
@@ -5,14 +5,33 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Add CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngularApp", policy =>
+    {
+        policy.WithOrigins("https://20.174.11.170") // Allow Angular app origin
+              .AllowAnyMethod()                     // Allow GET, POST, etc.
+              .AllowAnyHeader();                    // Allow any headers
+    });
+});
+
 var app = builder.Build();
 
 // Enable Swagger
 app.UseSwagger();
 app.UseSwaggerUI();
 
-// Enable routing
-app.UseHttpsRedirection();
+// Enable CORS
+app.UseCors("AllowAngularApp");
+
+// Enable HTTPS redirection only in production
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHttpsRedirection();
+}
+
 app.UseAuthorization();
 app.MapControllers();
+
 app.Run();
